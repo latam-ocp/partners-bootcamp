a74eebdc875e1db0e9f78b506bd6e3183bcc82d5
Gleisson Bezerra <gleissonbezerra@Gleissons-MacBook-Pro.local>
2018-09-05 20:43:39 -0300

    Customer Profile ASP.NET Core

--- Templates/LatamPartnerBootcampSFApp/CustomerProfileService/Program.cs
+++ Templates/LatamPartnerBootcampSFApp/CustomerProfileService/Program.cs
@@ -2,23 +2,43 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
+using Microsoft.ServiceFabric.Services.Runtime;
+
+
 namespace CustomerProfileService
 {
-    public class Program
+    public static class Program
     {
-        public static void Main(string[] args)
+        /// <summary>
+        /// This is the entry point of the service host process.
+        /// </summary>
+        public static void Main()
         {
-            CreateWebHostBuilder(args).Build().Run();
+            try
+            {
+           
+                ServiceRuntime.RegisterServiceAsync("CustomerProfileServiceType",
+                        context => new CustomerProfileService(context)).GetAwaiter().GetResult();
+
+                    ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(CustomerProfileService).Name);
+
+                    // Prevents this host process from terminating so services keeps running. 
+                    Thread.Sleep(Timeout.Infinite);
+            }
+            catch (Exception e)
+            {
+                ServiceEventSource.Current.ServiceHostInitializationFailed(e.ToString());
+                throw;
+            }
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
     }
 }
