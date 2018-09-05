9ca47d371828e98518ffdf8f1695ef18b2fdad3a
Gleisson Bezerra <gleissonbezerra@Gleissons-MacBook-Pro.local>
2018-09-05 20:29:46 -0300

    Customer Profile ASP.NET Core

--- Templates/LatamPartnerBootcampSFApp/CustomerProfileService/Program.cs
+++ Templates/LatamPartnerBootcampSFApp/CustomerProfileService/Program.cs
@@ -1,38 +1,24 @@
-﻿using Microsoft.ServiceFabric.Services.Runtime;
-using System;
-using System.Diagnostics;
-using System.Threading;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace CustomerProfileService
 {
-    internal static class Program
+    public class Program
     {
-        /// <summary>
-        /// This is the entry point of the service host process.
-        /// </summary>
-        private static void Main()
+        public static void Main(string[] args)
         {
-            try
-            {
-                // The ServiceManifest.XML file defines one or more service type names.
-                // Registering a service maps a service type name to a .NET type.
-                // When Service Fabric creates an instance of this service type,
-                // an instance of the class is created in this host process.
-
-                ServiceRuntime.RegisterServiceAsync("CustomerProfileServiceType",
-                    context => new CustomerProfileService(context)).GetAwaiter().GetResult();
-
-                ServiceEventSource.Current.ServiceTypeRegistered(Process.GetCurrentProcess().Id, typeof(CustomerProfileService).Name);
-
-                // Prevents this host process from terminating so services keeps running. 
-                Thread.Sleep(Timeout.Infinite);
-            }
-            catch (Exception e)
-            {
-                ServiceEventSource.Current.ServiceHostInitializationFailed(e.ToString());
-                throw;
-            }
+            CreateWebHostBuilder(args).Build().Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
